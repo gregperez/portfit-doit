33fc4fa04fe62c49148182b45e30fd4c5fc1743f
Greg Perez <ext_ghperezm@falabella.cl>
2021-12-14 17:52:07 -0300

    adding project structure

---
+++ src/main/java/com/gperez/portfitdoit/PortfitDoitApplication.java
@@ -0,0 +1,13 @@
+package com.gperez.portfitdoit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfitDoitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfitDoitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gperez/portfitdoit/PortfitDoitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gperez.portfitdoit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfitDoitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
